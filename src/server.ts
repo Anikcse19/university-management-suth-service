6454ece87724373b64c2ab76508035f0ec396428
Anik Chandra Deb <anikdebcse@gmail.com>
2023-07-14 17:23:26 +0600

    check

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import conifg from "./config/index"
-
+import mongoose from 'mongoose'
+import app from './app'
+import conifg from './config/index'
 
 async function bootstrap() {
   try {
-    await mongoose.connect(conifg.database_url as string);
-    console.log("Database is connected successfully");
+    await mongoose.connect(conifg.database_url as string)
+    console.log('Database is connected successfully')
     app.listen(conifg.Port, () => {
-      console.log(`Server listening at port ${conifg.Port}`);
-    });
+      console.log(`Server listening at port ${conifg.Port}`)
+    })
   } catch (err) {
-    console.log("Failed to connect", err);
+    console.log('Failed to connect', err)
   }
 }
 
-bootstrap();
+bootstrap()
